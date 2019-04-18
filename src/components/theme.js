a0624b7381710927e88c2c36c384bb3ff51ca31b
João Costa <joaocosta@MBP-de-Joao.lan>
2019-04-18 16:23:52 +0100

    initial commit

---
+++ src/components/theme.js
@@ -0,0 +1,13 @@
+export default {
+    buttons: {
+      primary: {
+        color: '#fff',
+        backgroundColor: '#00A86B',
+      },
+      outline: {
+        color: '#00A86B',
+        backgroundColor: 'transparent',
+        boxShadow: 'inset 0 0 0 2px'
+      }
+    }
+  }
